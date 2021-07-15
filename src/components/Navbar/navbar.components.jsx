a2df5c30f2d9c105a0ffb400c8f7d2de7e33fb2b
gayathri1398 <gayathrivasu13@gmail.com>
2021-07-15 23:04:25 +0530

    added navbar for medium devices

--- src/components/Navbar/navbar.components.jsx
+++ src/components/Navbar/navbar.components.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BiChevronRight,BiSearch} from 'react-icons/bi';
+import {BiChevronRight,BiSave,BiSearch} from 'react-icons/bi';
 
 const Navsm = () => {
     return<>
@@ -17,7 +17,17 @@ const Navsm = () => {
 
 };
 
-const Navmd = ()=> {
+const Navmd = ()=> {   
+    const style = { color: "grey"};
+    return<>
+   
+    <div className="w-full h-10  flex items-center bg-white p-2">
+    <div className=" text-grey-500 h-10"><BiSearch style={style} className="h-full"/></div>
+    
+    <input type="text" placeholder="  Search for Movies, Events, Plays, Sports and Activities" className="w-full " />
+     
+   </div>
+    </>
     
 };
 
@@ -28,9 +38,9 @@ const Navlg = ()=> {
 
 const Navbar = () => {
 return <>
-   <nav className="bg-bms-700">
+   <nav className="bg-bms-700 p-3">
     <div className="md:hidden">{/* Mobile devices */}<Navsm/></div>
-    <div className ="hidden md:flex lg:hidden">{/* Tablet devices */}</div>
+    <div className ="hidden md:flex lg:hidden">{/* Tablet devices */}<Navmd/></div>
     <div className ="hidden lg:flex">{/* PC devices */}</div>
    </nav>
 
