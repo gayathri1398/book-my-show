aa6458bbb40b5717a32d5d73b7589b3850bafb2c
gayathri1398 <gayathrivasu13@gmail.com>
2021-07-16 00:17:19 +0530

    added navbar for lg devices

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import React from "react";
 
 import DefaultHOC from "./HOC/Default.HOC";
 
--- src/components/Navbar/navbar.components.jsx
+++ src/components/Navbar/navbar.components.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BiChevronRight,BiSave,BiSearch} from 'react-icons/bi';
+import {BiChevronRight,BiSearch,BiChevronDown,BiMenu} from 'react-icons/bi';
 
 const Navsm = () => {
     return<>
@@ -18,7 +18,7 @@ const Navsm = () => {
 };
 
 const Navmd = ()=> {   
-    const style = { color: "grey"};
+    let style = { color: "grey"};
     return<>
    
     <div className="w-full h-10  flex items-center bg-white p-2">
@@ -32,7 +32,32 @@ const Navmd = ()=> {
 };
 
 const Navlg = ()=> {
-    
+    let style = { color: "grey"};
+    return<>
+    <div className="flex items-center justify-around w-full">
+            <div className ="flex items-center w-5/12 ">
+                <div className="w-10 h-10"> 
+                    <img src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png" alt="logo" className="h-8" />
+                </div>
+                <div className="w-full h-10  flex items-center bg-white p-2 rounded">
+                    <div className=" text-grey-500 h-10"><BiSearch style={style} className="h-full"/></div>
+                    
+                    <input type="text" placeholder="  Search for Movies, Events, Plays, Sports and Activities" className="w-full " />
+                    
+                </div>
+        </div>
+
+        <div className="flex items-center gap-4">
+                <span className="flex items-center text-white">Bengaluru <BiChevronDown/></span>
+                <div className ="bg-bms-900 rounded ">
+                    <h2 className="text-sm text-white border-2  border-none px-2 py-1 ">Sign In</h2>
+                </div>
+                <div className="w-6 h-6">
+                <BiMenu className="text-white w-full h-full"/>
+                </div>
+        </div>
+   </div>
+    </>
 }
 
 
@@ -41,7 +66,7 @@ return <>
    <nav className="bg-bms-700 p-3">
     <div className="md:hidden">{/* Mobile devices */}<Navsm/></div>
     <div className ="hidden md:flex lg:hidden">{/* Tablet devices */}<Navmd/></div>
-    <div className ="hidden lg:flex">{/* PC devices */}</div>
+    <div className ="hidden lg:flex">{/* PC devices */}<Navlg/></div>
    </nav>
 
 
